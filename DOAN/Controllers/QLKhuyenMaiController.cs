5568628db9c499f2b8ae0d8450839ffe04d1bab6
lluy4 <huymgiyee@gmail.com>
2025-03-16 21:55:33 +0700

    DECORATOR

--- DOAN/Controllers/QLKhuyenMaiController.cs
+++ DOAN/Controllers/QLKhuyenMaiController.cs
@@ -7,33 +7,33 @@ using DOAN.Models;
 using DOAN.Common;
 using System.Net;
 using System.Data.Entity;
-
+using DOAN.MTK.Decorator;
+//DECORATOR_HUY
 namespace DOAN.Controllers
 {
     [Authorize(Roles = "*")]
     public class QLKhuyenMaiController : Controller
     {
-        // GET: QLKhuyenMai
-        TMDTDbContext db = new TMDTDbContext();
-        public ActionResult Index(int error=0)
+        private readonly TMDTDbContext _context;
+
+        public QLKhuyenMaiController()
         {
-            var list = db.KHUYENMAIs.Where(x => x.TinhTrang == true).OrderByDescending(y=>y.NgayBD);
-            List<LoaiKM> listLoai = new List<LoaiKM>();
-            LoaiKM LOAI1 = new LoaiKM();
-            LOAI1.IdLoai = 1;
-            LOAI1.TenLoai = "Giảm phần trăm";
-            listLoai.Add(LOAI1);
-            LoaiKM LOAI2 = new LoaiKM();
-            LOAI2.IdLoai = 2;
-            LOAI2.TenLoai = "Giảm trực tiếp";
-            listLoai.Add(LOAI2);
+            _context = new TMDTDbContext();
+        }
+
+        // GET: QLKhuyenMai
+        public ActionResult Index(int error = 0)
+        {
+            var list = _context.KHUYENMAIs.Where(x => x.TinhTrang == true).OrderByDescending(y => y.NgayBD);
+            List<LoaiKM> listLoai = new List<LoaiKM>
+            {
+                new LoaiKM { IdLoai = 1, TenLoai = "Giảm phần trăm" },
+                new LoaiKM { IdLoai = 2, TenLoai = "Giảm trực tiếp" }
+            };
             ViewBag.items = new SelectList(listLoai, "IdLoai", "TenLoai");
             ViewBag.GiaTri = 0;
             ViewBag.DanhSach = list;
-
             ViewBag.Error = error;
-
-            
             return View(list);
         }
 
@@ -41,28 +41,24 @@ namespace DOAN.Controllers
         public ActionResult Index(FormCollection f)
         {
             var kq = f["ddlLoai"];
-            List<LoaiKM> listLoai = new List<LoaiKM>();
-            LoaiKM LOAI1 = new LoaiKM();
-            LOAI1.IdLoai = 1;
-            LOAI1.TenLoai = "Giảm phần trăm";
-            listLoai.Add(LOAI1);
-            LoaiKM LOAI2 = new LoaiKM();
-            LOAI2.IdLoai = 2;
-            LOAI2.TenLoai = "Giảm trực tiếp";
-            listLoai.Add(LOAI2);
+            List<LoaiKM> listLoai = new List<LoaiKM>
+            {
+                new LoaiKM { IdLoai = 1, TenLoai = "Giảm phần trăm" },
+                new LoaiKM { IdLoai = 2, TenLoai = "Giảm trực tiếp" }
+            };
 
             if (kq != "")
             {
                 int giatri = int.Parse(kq);
-                var list = db.KHUYENMAIs.Where(x => x.TinhTrang == true && x.LoaiKM==giatri).OrderByDescending(y => y.NgayBD);
+                var list = _context.KHUYENMAIs.Where(x => x.TinhTrang == true && x.LoaiKM == giatri).OrderByDescending(y => y.NgayBD);
                 ViewBag.DanhSach = list;
-                ViewBag.items = new SelectList(listLoai, "IdLoai", "TenLoai",giatri);
+                ViewBag.items = new SelectList(listLoai, "IdLoai", "TenLoai", giatri);
                 ViewBag.GiaTri = giatri;
                 return View(list);
             }
             else
             {
-                var list = db.KHUYENMAIs.Where(x => x.TinhTrang == true).OrderByDescending(y => y.NgayBD);
+                var list = _context.KHUYENMAIs.Where(x => x.TinhTrang == true).OrderByDescending(y => y.NgayBD);
                 ViewBag.DanhSach = list;
                 ViewBag.items = new SelectList(listLoai, "IdLoai", "TenLoai");
                 ViewBag.GiaTri = 0;
@@ -72,33 +68,48 @@ namespace DOAN.Controllers
 
         public ActionResult Create()
         {
-            List<LoaiKM> listLoai = new List<LoaiKM>();
-            LoaiKM LOAI1 = new LoaiKM();
-            LOAI1.IdLoai = 1;
-            LOAI1.TenLoai = "Giảm phần trăm";
-            listLoai.Add(LOAI1);
-            LoaiKM LOAI2 = new LoaiKM();
-            LOAI2.IdLoai = 2;
-            LOAI2.TenLoai = "Giảm trực tiếp";
-            listLoai.Add(LOAI2);
-
+            List<LoaiKM> listLoai = new List<LoaiKM>
+            {
+                new LoaiKM { IdLoai = 1, TenLoai = "Giảm phần trăm" },
+                new LoaiKM { IdLoai = 2, TenLoai = "Giảm trực tiếp" }
+            };
             ViewBag.LoaiKM = new SelectList(listLoai, "IdLoai", "TenLoai");
             return View();
         }
 
-
         [HttpPost]
         [ValidateInput(false)]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(KHUYENMAI khuyenmai)
+        public ActionResult Create(DOAN.Models.KHUYENMAI khuyenmai)
         {
-            khuyenmai.TinhTrang = true;
+            khuyenmai.TinhTrang = true; // Gán true cho bit, cần kiểm tra kiểu
             if (ModelState.IsValid)
             {
                 try
                 {
-                    db.KHUYENMAIs.Add(khuyenmai);
-                    db.SaveChanges();
+                    // Áp dụng Decorator để tính toán chiết khấu
+                    IKhuyenMai decoratedKhuyenMai = new DecoratedKhuyenMai(khuyenmai);
+                    decimal extraDiscount = 0;
+                    if (khuyenmai.LoaiKM.HasValue && khuyenmai.LoaiKM.Value == 1 && khuyenmai.GiaTri.HasValue && khuyenmai.GiaTri.Value > 20)
+                    {
+                        extraDiscount = 5m;
+                        decoratedKhuyenMai = new ExtraDiscountDecorator(decoratedKhuyenMai, extraDiscount);
+                    }
+
+                    // Tạo instance mới của KHUYENMAI để lưu vào database
+                    var newKhuyenMai = new DOAN.Models.KHUYENMAI
+                    {
+                        IdMa = decoratedKhuyenMai.IdMa,
+                        MaKM = decoratedKhuyenMai.MaKM,
+                        LoaiKM = decoratedKhuyenMai.LoaiKM,
+                        NgayBD = decoratedKhuyenMai.NgayBD,
+                        NgayKT = decoratedKhuyenMai.NgayKT,
+                        GiaTri = decoratedKhuyenMai.GiaTri,
+                        ChiTiet = decoratedKhuyenMai.ChiTiet,
+                        TinhTrang = decoratedKhuyenMai.TinhTrang
+                    };
+                    _context.KHUYENMAIs.Add(newKhuyenMai);
+                    _context.SaveChanges();
                     return RedirectToAction("Index");
                 }
                 catch (Exception)
@@ -110,17 +121,12 @@ namespace DOAN.Controllers
             {
                 ModelState.AddModelError("", "Vui lòng kiểm tra lại thông tin đã nhập.");
             }
-            List<LoaiKM> listLoai = new List<LoaiKM>();
-            LoaiKM LOAI1 = new LoaiKM();
-            LOAI1.IdLoai = 1;
-            LOAI1.TenLoai = "Giảm phần trăm";
-            listLoai.Add(LOAI1);
-            LoaiKM LOAI2 = new LoaiKM();
-            LOAI2.IdLoai = 2;
-            LOAI2.TenLoai = "Giảm trực tiếp";
-            listLoai.Add(LOAI2);
-
-            ViewBag.LoaiKM = new SelectList(listLoai, "IdLoai", "TenLoai",khuyenmai.LoaiKM);
+            List<LoaiKM> listLoai = new List<LoaiKM>
+            {
+                new LoaiKM { IdLoai = 1, TenLoai = "Giảm phần trăm" },
+                new LoaiKM { IdLoai = 2, TenLoai = "Giảm trực tiếp" }
+            };
+            ViewBag.LoaiKM = new SelectList(listLoai, "IdLoai", "TenLoai", khuyenmai.LoaiKM);
             return View(khuyenmai);
         }
 
@@ -131,32 +137,31 @@ namespace DOAN.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            KHUYENMAI khuyenmai = db.KHUYENMAIs.Find(id);
+            DOAN.Models.KHUYENMAI khuyenmai = _context.KHUYENMAIs.Find(id);
             if (khuyenmai == null)
             {
                 Response.StatusCode = 404;
                 return null;
             }
-            if(DateTime.Compare(DateTime.Now, khuyenmai.NgayBD ?? DateTime.Now) < 0)
+            if (DateTime.Compare(DateTime.Now, khuyenmai.NgayBD ?? DateTime.Now) < 0)
             {
                 try
                 {
                     khuyenmai.TinhTrang = false;
-                    db.Entry(khuyenmai).State = EntityState.Modified;
-                    db.SaveChanges();
+                    _context.Entry(khuyenmai).State = EntityState.Modified;
+                    _context.SaveChanges();
                     return RedirectToAction("Index");
-
                 }
                 catch (Exception ex)
                 {
                     string message = ex.Message;
                     return Content("<script> alert(\"Quá trình thực hiện thất bại.\")</script>");
                 }
-            }    
+            }
             else
             {
-                return RedirectToAction("Index","QLKhuyenMai",new {error=2});
-            }    
+                return RedirectToAction("Index", "QLKhuyenMai", new { error = 2 });
+            }
         }
 
         public ActionResult Edit(int? id)
@@ -165,43 +170,51 @@ namespace DOAN.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            KHUYENMAI khuyenmai = db.KHUYENMAIs.SingleOrDefault(x => x.IdMa == id);
+            DOAN.Models.KHUYENMAI khuyenmai = _context.KHUYENMAIs.SingleOrDefault(x => x.IdMa == id);
             if (khuyenmai == null)
             {
                 return HttpNotFound();
             }
             if (DateTime.Compare(DateTime.Now, khuyenmai.NgayBD ?? DateTime.Now) < 0)
             {
-                List<LoaiKM> listLoai = new List<LoaiKM>();
-                LoaiKM LOAI1 = new LoaiKM();
-                LOAI1.IdLoai = 1;
-                LOAI1.TenLoai = "Giảm phần trăm";
-                listLoai.Add(LOAI1);
-                LoaiKM LOAI2 = new LoaiKM();
-                LOAI2.IdLoai = 2;
-                LOAI2.TenLoai = "Giảm trực tiếp";
-                listLoai.Add(LOAI2);
-
+                List<LoaiKM> listLoai = new List<LoaiKM>
+                {
+                    new LoaiKM { IdLoai = 1, TenLoai = "Giảm phần trăm" },
+                    new LoaiKM { IdLoai = 2, TenLoai = "Giảm trực tiếp" }
+                };
                 ViewBag.LoaiKM = new SelectList(listLoai, "IdLoai", "TenLoai", khuyenmai.LoaiKM);
                 return View(khuyenmai);
-            }    
+            }
             else
             {
-                return RedirectToAction("Index","QLKhuyenMai", new {error=2});
-            }    
+                return RedirectToAction("Index", "QLKhuyenMai", new { error = 2 });
+            }
         }
 
         [HttpPost]
         [ValidateInput(false)]
-        public ActionResult Edit(KHUYENMAI khuyenmai)
+        public ActionResult Edit(DOAN.Models.KHUYENMAI khuyenmai)
         {
             khuyenmai.TinhTrang = true;
             if (ModelState.IsValid)
             {
                 try
                 {
-                    db.Entry(khuyenmai).State = EntityState.Modified;
-                    db.SaveChanges();
+                    // Áp dụng Decorator để tính toán chiết khấu
+                    IKhuyenMai decoratedKhuyenMai = new DecoratedKhuyenMai(khuyenmai);
+                    decimal extraDiscount = 0;
+                    if (khuyenmai.LoaiKM.HasValue && khuyenmai.LoaiKM.Value == 1 && khuyenmai.GiaTri.HasValue && khuyenmai.GiaTri.Value > 20)
+                    {
+                        extraDiscount = 5m;
+                        decoratedKhuyenMai = new ExtraDiscountDecorator(decoratedKhuyenMai, extraDiscount);
+                    }
+
+                    // Cập nhật instance hiện tại thay vì ép kiểu
+                    khuyenmai.LoaiKM = decoratedKhuyenMai.LoaiKM;
+                    khuyenmai.GiaTri = decoratedKhuyenMai.GiaTri;
+                    khuyenmai.ChiTiet = decoratedKhuyenMai.ChiTiet;
+                    _context.Entry(khuyenmai).State = EntityState.Modified;
+                    _context.SaveChanges();
                     return RedirectToAction("Index");
                 }
                 catch (Exception ex)
@@ -213,20 +226,13 @@ namespace DOAN.Controllers
             {
                 ModelState.AddModelError("", "Vui lòng kiểm tra lại thông tin đã nhập.");
             }
-
-            List<LoaiKM> listLoai = new List<LoaiKM>();
-            LoaiKM LOAI1 = new LoaiKM();
-            LOAI1.IdLoai = 1;
-            LOAI1.TenLoai = "Giảm phần trăm";
-            listLoai.Add(LOAI1);
-            LoaiKM LOAI2 = new LoaiKM();
-            LOAI2.IdLoai = 2;
-            LOAI2.TenLoai = "Giảm trực tiếp";
-            listLoai.Add(LOAI2);
-
-            ViewBag.LoaiKM = new SelectList(listLoai, "IdLoai", "TenLoai",khuyenmai.LoaiKM);
+            List<LoaiKM> listLoai = new List<LoaiKM>
+            {
+                new LoaiKM { IdLoai = 1, TenLoai = "Giảm phần trăm" },
+                new LoaiKM { IdLoai = 2, TenLoai = "Giảm trực tiếp" }
+            };
+            ViewBag.LoaiKM = new SelectList(listLoai, "IdLoai", "TenLoai", khuyenmai.LoaiKM);
             return View(khuyenmai);
         }
-
     }
 }
---
+++ DOAN/MTK/Decorator/KhuyenMaiDecorator.cs
@@ -0,0 +1,84 @@
+﻿using DOAN.Models;
+using System;
+//DECORATOR_HUY
+namespace DOAN.MTK.Decorator
+{
+    public interface IKhuyenMai
+    {
+        int IdMa { get; set; }
+        string MaKM { get; set; } // Sửa từ TenKM thành MaKM
+        DateTime? NgayBD { get; set; }
+        DateTime? NgayKT { get; set; }
+        int? LoaiKM { get; set; }
+        int? GiaTri { get; set; } // Sửa từ decimal? thành int? để khớp với database
+        string ChiTiet { get; set; } // Thêm ChiTiet
+        bool? TinhTrang { get; set; }
+        decimal CalculateDiscount(decimal originalPrice); // Giữ nguyên, tính toán dựa trên GiaTri (int?)
+    }
+
+    public class DecoratedKhuyenMai : IKhuyenMai
+    {
+        private readonly DOAN.Models.KHUYENMAI _khuyenMai;
+
+        public DecoratedKhuyenMai(DOAN.Models.KHUYENMAI khuyenMai)
+        {
+            _khuyenMai = khuyenMai ?? throw new ArgumentNullException(nameof(khuyenMai));
+        }
+
+        public int IdMa { get => _khuyenMai.IdMa; set => _khuyenMai.IdMa = value; }
+        public string MaKM { get => _khuyenMai.MaKM; set => _khuyenMai.MaKM = value; }
+        public DateTime? NgayBD { get => _khuyenMai.NgayBD; set => _khuyenMai.NgayBD = value; }
+        public DateTime? NgayKT { get => _khuyenMai.NgayKT; set => _khuyenMai.NgayKT = value; }
+        public int? LoaiKM { get => _khuyenMai.LoaiKM; set => _khuyenMai.LoaiKM = value; }
+        public int? GiaTri { get => _khuyenMai.GiaTri; set => _khuyenMai.GiaTri = value; } // Sửa thành int?
+        public string ChiTiet { get => _khuyenMai.ChiTiet; set => _khuyenMai.ChiTiet = value; }
+        public bool? TinhTrang { get => _khuyenMai.TinhTrang; set => _khuyenMai.TinhTrang = value; }
+
+        public virtual decimal CalculateDiscount(decimal originalPrice)
+        {
+            if (LoaiKM == null || GiaTri == null) return 0; // Kiểm tra null
+            if (LoaiKM == 1) // Giảm phần trăm
+                return originalPrice * ((decimal)GiaTri / 100); // Ép kiểu int? sang decimal
+            else if (LoaiKM == 2) // Giảm trực tiếp
+                return (decimal)GiaTri; // Ép kiểu int? sang decimal
+            return 0;
+        }
+    }
+
+    public abstract class KhuyenMaiDecorator : IKhuyenMai
+    {
+        protected IKhuyenMai _khuyenMai;
+
+        public KhuyenMaiDecorator(IKhuyenMai khuyenMai)
+        {
+            _khuyenMai = khuyenMai ?? throw new ArgumentNullException(nameof(khuyenMai));
+        }
+
+        public int IdMa { get => _khuyenMai.IdMa; set => _khuyenMai.IdMa = value; }
+        public string MaKM { get => _khuyenMai.MaKM; set => _khuyenMai.MaKM = value; }
+        public DateTime? NgayBD { get => _khuyenMai.NgayBD; set => _khuyenMai.NgayBD = value; }
+        public DateTime? NgayKT { get => _khuyenMai.NgayKT; set => _khuyenMai.NgayKT = value; }
+        public int? LoaiKM { get => _khuyenMai.LoaiKM; set => _khuyenMai.LoaiKM = value; }
+        public int? GiaTri { get => _khuyenMai.GiaTri; set => _khuyenMai.GiaTri = value; }
+        public string ChiTiet { get => _khuyenMai.ChiTiet; set => _khuyenMai.ChiTiet = value; }
+        public bool? TinhTrang { get => _khuyenMai.TinhTrang; set => _khuyenMai.TinhTrang = value; }
+
+        public virtual decimal CalculateDiscount(decimal originalPrice) => _khuyenMai.CalculateDiscount(originalPrice);
+    }
+
+    public class ExtraDiscountDecorator : KhuyenMaiDecorator
+    {
+        private readonly decimal _extraDiscount;
+
+        public ExtraDiscountDecorator(IKhuyenMai khuyenMai, decimal extraDiscount) : base(khuyenMai)
+        {
+            _extraDiscount = extraDiscount;
+        }
+
+        public override decimal CalculateDiscount(decimal originalPrice)
+        {
+            decimal baseDiscount = _khuyenMai.CalculateDiscount(originalPrice);
+            return baseDiscount + _extraDiscount; // Thêm chiết khấu bổ sung
+        }
+    }
+}
